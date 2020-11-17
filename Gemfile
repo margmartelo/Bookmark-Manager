116c44e263dffa116626d45c9c158687f7351fc1
Margarida Martelo <margmartelo@gmail.com>
2020-11-17 15:35:49 +0000

    Bookmark class takes bookmarks from database

--- Gemfile
+++ Gemfile
@@ -11,3 +11,4 @@ gem 'rspec'
 gem 'rubocop', '0.79.0'
 gem 'simplecov', require: false
 gem 'simplecov-console', require: false
+gem 'pg'
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,7 +1,9 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    ['http://www.reddit.com',
-     'http://www.bbc.co.uk',
-     'http://www.twitter.com']
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks;')
+    result.map { |bookmark| bookmark['url'] }
   end
 end
